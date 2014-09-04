53a337fd3e9683c3febd3cb4abc59332e87710ae
Randy Leighton <student@epicodus.com>
2014-09-03 18:09:50 -0700

    drug has many prescriptions relationship

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -3,5 +3,7 @@ class Drug < ActiveRecord::Base
 validates :name, presence: true, uniqueness: true
 validates :description, presence: true
 
+has_many :prescriptions
+
 end
 
--- spec/drug_spec.rb
+++ spec/drug_spec.rb
@@ -5,6 +5,7 @@ describe Drug do
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name }
   it { should validate_presence_of :description}
+  it {should have_many :prescriptions}
 
 
 end
