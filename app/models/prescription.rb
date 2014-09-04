3bc660a600732d76669bd7f8f05afd23b3b9d82e
Randy Leighton <student@epicodus.com>
2014-09-03 18:35:05 -0700

    prescrip validate cost in donut hole

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,6 +3,7 @@ class Prescription < ActiveRecord::Base
 validates :drug_uom, presence: true
 validates :quantity, presence: true
 validates :cost_not_in_dh, presence: true
+validates :cost_in_dh, presence: true
 
 
 belongs_to :drug
