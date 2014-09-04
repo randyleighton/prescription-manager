ecb2449adb291b740064104ac2af797a1fb940ac
Randy Leighton <student@epicodus.com>
2014-09-03 18:35:48 -0700

    prescrip validate bill to donut hole

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -4,6 +4,7 @@ validates :drug_uom, presence: true
 validates :quantity, presence: true
 validates :cost_not_in_dh, presence: true
 validates :cost_in_dh, presence: true
+validates :bill_to_dh, presence: true
 
 
 belongs_to :drug
