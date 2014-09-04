379d5aa90fe99f02d73fc6069e963955ede860cb
Randy Leighton <student@epicodus.com>
2014-09-03 18:08:52 -0700

    validate drug description

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,6 +1,7 @@
 class Drug < ActiveRecord::Base
 
 validates :name, presence: true, uniqueness: true
+validates :description, presence: true
 
 end
 
--- spec/drug_spec.rb
+++ spec/drug_spec.rb
@@ -4,6 +4,7 @@ describe Drug do
 
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name }
+  it { should validate_presence_of :description}
 
 
 end
