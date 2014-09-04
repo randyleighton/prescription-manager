e1648b9392558dbbbcba787a001260505c8257be
Randy Leighton <student@epicodus.com>
2014-09-03 18:36:12 -0700

    prescrip validate date

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -5,7 +5,7 @@ validates :quantity, presence: true
 validates :cost_not_in_dh, presence: true
 validates :cost_in_dh, presence: true
 validates :bill_to_dh, presence: true
-
+validate :date, presence: true
 
 belongs_to :drug
 
