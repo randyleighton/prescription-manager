428935f75f6c4e6cfb58d12e8d5e374a7c7d8890
Randy Leighton <student@epicodus.com>
2014-09-03 19:02:32 -0700

    validate date fixed and index for drug working

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,3 +1,29 @@
 class DrugsController < ApplicationController
 
+  def index
+    @drugs = Drug.all
+  end
+
+  def new
+    @drug = Drug.new
+
+  end
+
+  def create
+
+  end
+
+
+  def edit
+
+  end
+
+
+  def update
+  end
+
+  def destroy
+
+  end
+
 end
