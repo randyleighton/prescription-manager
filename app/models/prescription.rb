00f7c5f0ae264b3a1d6aa3672af64e89dd2715bd
Randy Leighton <student@epicodus.com>
2014-09-03 18:31:21 -0700

    prescrip validate uom

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,5 +1,9 @@
 class Prescription < ActiveRecord::Base
 
+validates :drug_uom, presence: true
+
+
+
 belongs_to :drug
 
 end
--- spec/prescription_spec.rb
+++ spec/prescription_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe Prescription do
 
-it { should belong_to :drug}
+it { should validate_presence_of :drug_uom }
 
+it { should belong_to :drug}
 
 end
