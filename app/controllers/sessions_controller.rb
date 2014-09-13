4c804ad9146b1e9fa782093181f2fe4be8ffbd90
Randy Leighton <randyleighton@yahoo.com>
2014-09-13 19:18:41 +0000

    authentication working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,16 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user), notice: "logged in!"
+      redirect_to root_path, notice: "Logged in!"
     else
-      flash.now.alert = "email or password is invalid"
+      flash.now.alert = "Name or Password is Invalid"
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "logged out"
+    redirect_to root_path, notice: "Logged out"
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user), notice: "Thank you for signing up!"
+      redirect_to root_path(@user), notice: "Account Created."
     else
       render "new"
     end
   end
 
   def show
-    @questions = current_user.questions
+    
   end
 
 
