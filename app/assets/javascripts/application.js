75009779425b9718ceda10918e828806466ca9c0
Randy Leighton <randyleighton@yahoo.com>
2014-09-13 22:46:04 +0000

    ui improvement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require main_app
+//= require bootstrap/dropdown
+//= require bootstrap/transition
+//= require bootstrap/collapse
