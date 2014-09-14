09766c607b39b6778bfd5c662673470135347e7d
Randy Leighton <randyleighton@yahoo.com>
2014-09-14 01:28:57 +0000

    prescription belongs to user

--- spec/prescription_spec.rb
+++ spec/prescription_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe Prescription do
 
-it { should validate_presence_of :drug_uom }
-it { should validate_presence_of :renewal_interval }
-
-it { should belong_to :drug}
-it { should have_many :ledger_prescriptions}
+  it { should validate_presence_of :drug_uom }
+  it { should validate_presence_of :renewal_interval }
+  it { should belong_to :drug}
+  it { should have_many :ledger_prescriptions}
+  it { should belong_to :user }
 
 end
