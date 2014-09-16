8e661e1b83953583e2ef721b85d31ce05fc6d747
Randy Leighton <student@epicodus.com>
2014-09-15 18:24:01 -0700

    capybara - user fails to confirm password can not sign up

--- spec/features/authenticate_spec.rb
+++ spec/features/authenticate_spec.rb
@@ -11,6 +11,17 @@ describe "user sign in" do
     expect(page).to have_content 'Logged in as nacho'
   end
 
+  it "will not allow a user to sign up without all information" do
+    visit '/signup'
+    fill_in 'Name', with: 'nacho'
+    fill_in 'Password', with: 'testpassword'
+    fill_in 'Password confirmation', with: 'nopassword'
+    click_button 'Create User'
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+
+
+
   it "will take an existing user to the log in page" do
     visit '/'
     click_link 'Log In'
