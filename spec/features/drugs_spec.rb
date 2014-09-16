72b16030fb12bdfe2d94c0e393901d7092c98ca8
Randy Leighton <student@epicodus.com>
2014-09-15 20:20:43 -0700

    allow user to visit drugs page after authenticate

--- spec/features/drugs_spec.rb
+++ spec/features/drugs_spec.rb
@@ -6,14 +6,33 @@ describe "drug authentication" do
     find(:xpath, "//a[contains(@href,'/drugs')]").click
     expect(page).to have_content 'Log In'
   end
+  it "will allow a user to access drug page if logged in" do
+    user = User.create(name: 'nacho', password: 'testpassword')
+    visit '/login'
+    fill_in 'Name', with: 'nacho'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log In'
+    visit '/drugs'
+    expect(page).to have_content 'Current Drugs:'
+  end
 end
-  # it "will allow a user to access drug page if logged in" do
-  #   user = User.create(name: 'nacho', password: 'testpassword')
-
-  # end
 
 # describe "adding drugs" do
 #   it "allows user to create a new drug if all information is entered" do
+    # user = User.create(name: 'nacho', password: 'testpassword')
+    # visit '/login'
+    # fill_in 'Name', with: 'nacho'
+    # fill_in 'Password', with: 'testpassword'
+    # click_button 'Log In'
+    # visit '/drugs'
+    # find(:xpath, "//a[contains(@href,'/drugs/new')]").click
+    # fill_in 'Name', with: 'Norco'
+    # fill_in 'Description', with: 'Hydrocodone Generic for pain'
+    # fill_in 'Cost_not_in_dh', with: '10'
+    # fill_in 'Cost_in_dh', with: '30'
+    # fill_in 'Bill_to_dh', with: '20'
+    # click_button 'Create Drug'
+    # expect(page).to have_content 'Norco created successfully'
 
 #   end
 #   it "will not allow a user to create a new drug if missing information" do
