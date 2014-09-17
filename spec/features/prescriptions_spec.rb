a5ad2e2620269f54be35b0cb1b60a22aad371cfd
Randy Leighton <student@epicodus.com>
2014-09-16 18:43:10 -0700

    capybara test: will allow a user to access prescription page if logged in

--- spec/features/prescriptions_spec.rb
+++ spec/features/prescriptions_spec.rb
@@ -7,4 +7,13 @@ describe "prescription authentication" do
     expect(page).to have_content 'Log In'
   end
 
+  it "will allow a user to access prescription page if logged in" do
+    user = User.create(name: 'nacho', password: 'testpassword')
+    visit '/login'
+    fill_in 'Name', with: 'nacho'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log In'
+    visit '/prescriptions'
+    expect(page).to have_content 'Current Prescriptions:'
+  end
 end
