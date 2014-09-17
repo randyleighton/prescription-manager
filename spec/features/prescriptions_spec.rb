64ec7253d8a7fa03cdb67d3bb8f36d6d01405fce
Randy Leighton <student@epicodus.com>
2014-09-16 18:50:14 -0700

    capybara test:redirects a user to drugs page if none exist

--- spec/features/prescriptions_spec.rb
+++ spec/features/prescriptions_spec.rb
@@ -6,7 +6,6 @@ describe "prescription authentication" do
     find('img#prescrip-main').click
     expect(page).to have_content 'Log In'
   end
-
   it "will allow a user to access prescription page if logged in" do
     user = User.create(name: 'nacho', password: 'testpassword')
     visit '/login'
@@ -16,4 +15,22 @@ describe "prescription authentication" do
     visit '/prescriptions'
     expect(page).to have_content 'Current Prescriptions:'
   end
+  it "redirects a user to drugs page if none exist" do
+    user = User.create(name: 'nacho', password: 'testpassword')
+    visit '/login'
+    fill_in 'Name', with: 'nacho'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log In'
+    visit '/prescriptions'
+    click_on 'New Prescription'
+    expect(page).to have_content 'Create drugs to use in your prescriptions'
+  end
+
 end
+
+
+
+describe "adding prescriptions" do
+
+end
+
