40fb2a14de5422b55414299c26b6ae4a748ac6cd
Randy Leighton <student@epicodus.com>
2014-09-16 19:30:39 -0700

    capybara test: views the information for an individual prescription when a user clicks on it

--- spec/features/prescriptions_spec.rb
+++ spec/features/prescriptions_spec.rb
@@ -25,7 +25,6 @@ describe "prescription authentication" do
     click_on 'New Prescription'
     expect(page).to have_content 'Create drugs to use in your prescriptions'
   end
-
 end
 
 describe "adding prescriptions" do
@@ -46,8 +45,7 @@ describe "adding prescriptions" do
     click_button 'Create Prescription'
     expect(page).to have_content 'Norco prescription created successfully.'
   end
-
-it "will not create a new prescription unless all information is entered correctly" do
+  it "will not create a new prescription unless all information is entered correctly" do
     user = User.create(name: 'nacho', password: 'testpassword')
     drug = Drug.create(name: 'Norco', description: 'Strong Stuff', cost_not_in_dh: 10, cost_in_dh: 10, bill_to_dh: 10)
     visit '/login'
@@ -64,7 +62,26 @@ it "will not create a new prescription unless all information is entered correct
     click_button 'Create Prescription'
     expect(page).to have_content "Renewal interval can't be blank"
   end
+end
+
+describe "individual prescriptions" do
+  it "views the information for an individual prescription when a user clicks on it" do
+    user = User.create(name: 'nacho', password: 'testpassword')
+    drug = Drug.create(name: 'Norco', description: 'Strong Stuff', cost_not_in_dh: 10, cost_in_dh: 10, bill_to_dh: 10)
+    prescrip = Prescription.create(drug_id: drug.id, user_id: user.id, drug_uom: 1, renewal_interval: 1)
+    visit '/login'
+    fill_in 'Name', with: 'nacho'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log In'
+    visit '/prescriptions'
+    click_link 'Norco'
+    expect(page).to have_content 'Delete this prescription'
+  end
+
+
+
+
+
 
 
 end
-
