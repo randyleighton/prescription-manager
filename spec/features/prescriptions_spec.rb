c716a43b894cfe8c33044f6c416cb9689b5d34d5
Randy Leighton <student@epicodus.com>
2014-09-16 19:23:55 -0700

    capybara test: will not create a new prescription unless all information is entered correctly

--- spec/features/prescriptions_spec.rb
+++ spec/features/prescriptions_spec.rb
@@ -47,5 +47,24 @@ describe "adding prescriptions" do
     expect(page).to have_content 'Norco prescription created successfully.'
   end
 
+it "will not create a new prescription unless all information is entered correctly" do
+    user = User.create(name: 'nacho', password: 'testpassword')
+    drug = Drug.create(name: 'Norco', description: 'Strong Stuff', cost_not_in_dh: 10, cost_in_dh: 10, bill_to_dh: 10)
+    visit '/login'
+    fill_in 'Name', with: 'nacho'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log In'
+    visit '/prescriptions'
+    click_on 'New Prescription'
+    select 'Norco', :from => 'prescription_drug_id'
+    find_field('Drug uom')
+    fill_in 'Drug uom', with: '1'
+    # find_field('Renewal interval')
+    # fill_in 'Renewal interval', with: '1'
+    click_button 'Create Prescription'
+    expect(page).to have_content "Renewal interval can't be blank"
+  end
+
+
 end
 
