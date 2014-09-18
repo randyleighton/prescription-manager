4b6958f2fa59fbfdbbe28f41ab514618f4ff2478
Randy Leighton <student@epicodus.com>
2014-09-18 13:49:46 -0700

    angular change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.js_compressor = Uglifier.new(mangle: false)
+  # config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = Uglifier.new(mangle: false)
 
   # config.assets.css_compressor = :sass
 
