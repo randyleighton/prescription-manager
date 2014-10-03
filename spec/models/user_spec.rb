355c21bbbc49362f5864c4ca30275c1b587c2da8
Randy Leighton <randyleighton@yahoo.com>
2014-10-03 18:13:08 +0000

    cleanup css remove missing and move some spec files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  
+#   it { should validate_uniqueness_of :name }
+  it { should have_many :prescriptions }
+  
 end
