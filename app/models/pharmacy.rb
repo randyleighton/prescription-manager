acfcc1d9c44dc04be964d215244def4641d6e643
Randy Leighton <randyleighton@yahoo.com>
2014-12-29 11:06:27 -0800

    update pharmacy test for unique name, new test for before save add url http. New feature broke unique name because it was not validating for nil value before trying to run method on nil class if it was not there

--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -3,4 +3,14 @@ class Pharmacy < ActiveRecord::Base
   has_many :fillings
   validates :name, presence: true, uniqueness: true
 
+  before_save :external_link, :reject_if => :url_blank
+
+private
+
+  def external_link
+    if self.url!= nil && !self.url.include?("http://")
+      self.url.insert(0,"http://")
+    end
+  end
+
 end
--- spec/models/pharmacy_spec.rb
+++ spec/models/pharmacy_spec.rb
@@ -4,4 +4,12 @@ it { should have_many :fillings }
 it { should validate_presence_of :name }
 it { should validate_uniqueness_of :name }
 
+it "should add http:// to url if missing" do
+  pharmacy = Pharmacy.create(name:"Walgreens", url:"www.nachos.com")
+  expect(pharmacy.url).to eq "http://www.nachos.com"
+end
+
+
+
+
 end
