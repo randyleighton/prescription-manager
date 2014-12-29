aeef376decb6fbc952d1278535aefd7f41061576
Randy Leighton <randyleighton@yahoo.com>
2014-12-29 01:13:53 -0800

    create pharmacy controller

---
+++ app/controllers/pharmacy_controller.rb
@@ -0,0 +1,53 @@
+class DrugsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :find_pharmacy, except: [:index, :new, :create]
+
+  def index
+    @pharmacies = Pharmacy.all.where(user.id:current_user.id)
+  end
+
+  def new
+    @pharmacy = Pharmacy.new
+  end
+
+  def create
+    @pharmacy = Pharmacy.new(pharmacy_params)
+    if @pharmacy.save
+      flash[:notice] = "#{@pharmacy.name} created successfully."
+      redirect_to pharmacies_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pharmacy.update_attributes(pharmacy_params)
+      redirect_to pharmacy_path(@pharmacy), notice: "#{@pharmacy.name} updated successfully."
+    else
+      render 'edit', notice: "Failed to Update."
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+      flash[:notice] = "Removed Successfully"
+    @pharmacy.destroy
+    redirect_to pharmacies_path
+  end
+
+private
+
+  def pharmacy_params
+    params.require(:drug).permit(:name, :url, :phone, :user_id)
+  end
+
+  def find_pharmacy
+        @pharmacy = Pharmacy.find(params[:id])
+  end
+
+end
