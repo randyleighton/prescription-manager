6c3b0f7c92bb047708caa65afd8ac257bc9be844
Randy Leighton <randyleighton@yahoo.com>
2015-01-17 13:40:03 -0800

    update forms for new migration

--- app/models/filling.rb
+++ app/models/filling.rb
@@ -7,7 +7,6 @@ belongs_to :user
 validates :pharmacy_id, presence: true
 validates :prescription_id, presence: true
 validates :user_paid, presence: true
-validates :medicare_paid, presence: true
 
 accepts_nested_attributes_for :pharmacy, :reject_if => :all_blank
 
--- spec/models/filling_spec.rb
+++ spec/models/filling_spec.rb
@@ -2,7 +2,6 @@ describe Filling do
 
 it { should belong_to :prescription }
 it { should belong_to :pharmacy }
-it { should validate_presence_of :medicare_paid }
 it { should validate_presence_of :user_paid}
 it { should validate_presence_of :pharmacy_id }
 it { should validate_presence_of :prescription_id }
