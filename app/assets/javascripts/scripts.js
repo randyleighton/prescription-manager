ecdd4b5ef757044e3ba523c82cd37624c3d7c61f
Randy Leighton <randyleighton@yahoo.com>
2015-02-05 23:25:58 -0800

    add support to prescription form for add new doctor button, also to the scripts file

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
     $('#flash_notice').remove();
   }, 5000);
 
-
   $('#create-new-drug').click(function(){
     $('#prescription_drug_id').toggle();
     $('#choose-or').toggle();
@@ -14,24 +13,20 @@ $(document).ready(function () {
     $('#iconChange').toggleClass('fa-chevron-down').toggleClass('fa-chevron-up');
   }); 
 
+  $('#create-new-doc').click(function(){
+    $('#prescription_doctor_id').toggle();
+    $('#choose-or').toggle();
+    $('#create-new-doc').toggle();
+    $('.add-new-doc').toggle();
+    $('#iconChange').toggleClass('fa-chevron-down').toggleClass('fa-chevron-up');
+  }); 
+
   $('#add-pharm-address').click(function(){
     $('#add-address').toggle();
     $(this).remove();
   }); 
 
-  // $('#filling_pharmacy_id').click(function() {
-  //   event.preventDefault;
-  //   alert("selection element clicked");
-  //   $('#create-pharmacy').toggle();
-  // })
-
   $('#create-pharmacy').click(function(){
-    // $('#filling_pharmacy_id').children().remove().end().append('<option selected value="Choose Existing Pharmacy">Choose Existing Pharmacy</option>') ;
-
-    // $('form#new_filling').find('filling[pharmacy_id]').removeAttr('selected');
-
-    // $("select[name='filling_pharmacy_id']").find("option[value='Choose an Existing Pharmacy']").attr("selected","selected");
-
     $('#filling_pharmacy_id').toggle();
     $('#choose-or').toggle();
     $('.add-pharm').toggle();
