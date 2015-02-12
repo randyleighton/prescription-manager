f17822aded4752dd7061b6f5ba8cb245eb34489f
Randy Leighton <randyleighton@yahoo.com>
2015-02-11 20:03:41 -0800

    update controller and doctor show page

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,53 @@
+class DoctorsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :find_doctor, except: [:index, :new, :create]
+
+  def index
+    @doctors = Doctor.all.where(user_id: current_user.id)
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.valid?
+      redirect_to doctors_path, notice: "#{@doctor.name} Created Successfully"
+    else
+      render 'new', notice: "Failed to create"
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @doctor.update(doctor_params)
+      redirect_to doctor_path(doctor), notice: "#{@doctor.name} Updated Successfully"
+    else
+      render 'edit', notice: "Failed to Update"
+    end
+  end
+
+  def destroy
+    flash[:notice]= "Removed Successfully"
+  end
+
+private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :office_phone, :practice_name, :email, :user_id)
+  end
+
+  def find_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+
+end
